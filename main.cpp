490638557725c1277515388d873f590780eb8d7b
saleenkai <80540606+saleenkai@users.noreply.github.com>
2022-06-05 14:30:33 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Saleen Tulinski on 6/5/22.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Hello, World!";
+    return 0;
+}
