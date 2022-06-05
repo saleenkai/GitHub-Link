d0ebd0548597a16040eec54d300a251a476c6b09
Saleen Tulinski <saleenkai@gmail.com>
2022-06-05 14:15:56 -0400

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Saleen Tulinski on 6/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
